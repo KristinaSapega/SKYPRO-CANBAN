e022a2cbb2ee751d43842763f7208ea092de2adc
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2024-09-27 23:29:11 +0300

    hook added

---
+++ vite-project/src/hooks/useUser.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const getUserFromLocalStorage = () => {
+    try {
+        const user = JSON.parse(localStorage.getItem("user"));
+        return user && user.token ? user : null; 
+    }catch {
+        return null;
+    }
+};
+
+export const useUser = () => {
+    const [user, setUser] = useState(getUserFromLocalStorage());
+
+    const login = (userData) => {
+        setUser(userData);
+    };
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+    };
+
+    return (user, login, logout, setUser)
+}
