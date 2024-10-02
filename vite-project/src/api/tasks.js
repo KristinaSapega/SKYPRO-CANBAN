58d448fc5f033cfe4ab8e7198676f0a84c3c0f26
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2024-10-02 23:59:42 +0300

    added api addTask

--- vite-project/src/api/tasks.js
+++ vite-project/src/api/tasks.js
@@ -12,3 +12,20 @@ export const getTasks = async (token) => {
     }
     return response.json();
 };
+
+export const addTask = async (token) => {
+    const response = await fetch (apiURL, {
+        method: "POST",
+        headers: {
+            Authorization:  `Bearer ${token}`
+        },
+        body: JSON.stringify()
+    });
+
+    if (!response.ok) {
+        throw new Error ("Ошибка, не удалось добавить задачу. Попробуйте позже.");
+    }
+    return response.json();
+};
+
+
